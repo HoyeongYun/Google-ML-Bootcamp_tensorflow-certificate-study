85df96391893999d58d414ff939b3a7990328d8e
yunhoyeong <ddolddori12@naver.com>
2022-07-21 14:01:44 +0900

    data augmentation code For Horse Data Set

---
+++ CNN_In_Tensorflow/W2/W2_Lab2_Augmentation_Horse_Dataset.py
@@ -0,0 +1,19 @@
+import zipfile
+import wget
+import os
+import tensorflow as tf
+
+
+# url1 = 'https://storage.googleapis.com/tensorflow-1-public/course2/week3/horse-or-human.zip'
+# url2 = 'https://storage.googleapis.com/tensorflow-1-public/course2/week3/validation-horse-or-human.zip'
+# wget.download(url1)
+# wget.download(url2)
+#
+# zip_ref = zipfile.ZipFile('./horse-or-human.zip', 'r')
+# zip_ref.extractall('./horse-or-human')
+#
+# zip_ref = zipfile.ZipFile('./validation-horse-or-human.zip', 'r')
+# zip_ref.extractall('./validation-horse-or-human')
+#
+# zip_ref.close()
+
