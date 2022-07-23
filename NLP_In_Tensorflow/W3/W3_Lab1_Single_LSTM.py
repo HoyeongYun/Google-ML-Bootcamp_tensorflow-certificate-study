87c13b75fa943908fe76247965d6b5f09796ce8b
yunhoyeong <ddolddori12@naver.com>
2022-07-24 01:03:57 +0900

    LSTM study

---
+++ NLP_In_Tensorflow/W3/W3_Lab1_Single_LSTM.py
@@ -0,0 +1,51 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import matplotlib.pyplot as plt
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
+
+tokenizer = info.features['text'].encoder
+
+print(info.features)
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 100        # 유튜브 영상 보니 명확해짐 https://www.youtube.com/watch?v=c7G5W4Wv72Q
+
+train_data, test_data = dataset['train'], dataset['test']
+train_dataset = train_data.shuffle(BUFFER_SIZE)     # test data는 shuffle 할 필요가 없다
+
+train_dataset = train_dataset.padded_batch(BATCH_SIZE)
+test_dataset = test_data.padded_batch(BATCH_SIZE)
+
+embedding_dim = 64
+lstm_dim = 64
+dense_dim = 64
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(tokenizer.vocab_size, embedding_dim),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_dim)),
+    tf.keras.layers.Dense(dense_dim, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+NUM_EPOCHS = 10
+
+history = model.fit(train_dataset, validation_data=test_dataset, epochs=NUM_EPOCHS)
+
+def plot_graphs(hitory, string):
+    plt.plot(history.history[string])
+    plt.plot(history.history['val_' + string])
+
+    plt.xlabel('EPOCHS')
+    plt.ylabel(string)
+
+    plt.legend([string, 'val_' + string])
+    plt.show()
+
+plot_graphs(history, 'accuracy')
+plot_graphs(history, 'loss')
