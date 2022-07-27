9ed3a032934c020eb405583641ff7a0a9670d819
yunhoyeong <ddolddori12@naver.com>
2022-07-27 23:16:22 +0900

    W4 C4 Lab3

--- Sequences_TimeSeries_Prediction/W4/W4_Lab4 Sunspots CNN+RNN+DNN.py
+++ Sequences_TimeSeries_Prediction/W4/W4_Lab4 Sunspots CNN+RNN+DNN.py
@@ -3,4 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-def plot_series(x, y)
+def plot_series(x, y, format='-', start=0, end=None, title=None, xlabel=None, ylabel=None, legend=None):
+    plt.figure(figsize=(10, 6))
+
+    if type(y) is tuple:
+        for y_curr in y:
+            plt.plot(x[start:end], y_curr[start:end], format)
+    else:
+        plt.plot(x[start:end], y[start:end], format)
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(True)
+    plt.title(title)
+
+    if legend:
+        plt.legend(legend)
+
+url = 'https://storage.googleapis.com/tensorflow-1-public/course4/Sunspots.csv'
+wget.download(url)
+
+time_step = []
+sunspots = []
+
+# with open('')
