c143dfed107aa4459a168d030a8cc428569b9571
yunhoyeong <ddolddori12@naver.com>
2022-07-27 23:59:52 +0900

    W4 C4 Lab3

--- Sequences_TimeSeries_Prediction/W4/W4_Lab4 Sunspots CNN+RNN+DNN.py
+++ Sequences_TimeSeries_Prediction/W4/W4_Lab4 Sunspots CNN+RNN+DNN.py
@@ -96,3 +96,46 @@ model.compile(loss=tf.keras.losses.Huber(), optimizer=optimizer)
 
 history = model.fit(train_set, epochs=100, callbacks=[lr_schedule])
 
+# plot results (lr vs loss)
+lrs = 1e-8 * (10 ** (np.arange(100)/20))
+
+plt.figure(figsize=(10, 6))
+
+plt.semilogx(lrs, history.history['loss'])
+
+plt.tick_params('both', width=1, legth=10, which='both')
+
+plt.axis([1e-8, 1e-3, 0, 100])
+
+plt.show()
+
+# lr = 8e-7 찾은 lr로 model retraining, 아까 조건과 똑같이 시작하기 위해 save한 weight이용
+tf.keras.backend.clear_session()
+
+model.set_weigths(init_weights)
+
+learning_rate = 8e-7
+
+optimizer = tf.keras.optimizers.SGD(learning_rate, momentum=0.9)
+
+model.compile(loss=tf.keras.losses.Huber, optimizer=optimizer, metrics=['mae'])
+
+history = model.fit(train_set, epochs=100)
+
+# plot mae vs loss -> 둘다 감소하는 거 확인 -> 보다 좁은 range에서 자세히 확인
+mae = history.history['mae']
+loss = history.history['loss']
+
+epochs = range(len(loss))
+
+plot_series(x=epochs, y=(mae, loss), title='MAE and Loss', xlabel='MAE', ylabel='Loss', legend=['MAE', 'Loss'])
+
+zoom_split = int(epochs[-1] * 0.2)
+epochs_zoom = epochs[zoom_split:]
+mae_zoom = mae[zoom_split:]
+loss_zoom = loss[zoom_split:]
+
+plot_series(x=epochs_zoom, y=(mae_zoom, loss_zoom), title='MAE and Loss', xlabel='MAE', ylabel='Loss', legend=['MAE', 'Loss'])
+
+# Model Prediction
+def model_forecast(model,)
